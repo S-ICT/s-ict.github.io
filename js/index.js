b67896731d632917347f7b84b1f2020a60b79926
vicchen0321 <victor72441299@gmail.com>
2024-06-24 01:05:27 +0800

    first version

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document
+  .getElementById("claim-prize-button")
+  .addEventListener("click", function () {
+    setTimeout(function () {
+      window.location.href = "main.html";
+    }, 0);
+  });
